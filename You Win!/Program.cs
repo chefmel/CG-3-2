862c655a8197e02bc0bed3ba4d8537f5c540fb0e
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-08-04 09:45:46 -0500

    did some changes

--- You Win!/Program.cs
+++ You Win!/Program.cs
@@ -4,9 +4,36 @@ namespace You_Win_
 {
     class Program
     {
+        private static string classify;
+        private static string somevalue;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("pick a number between 1-10");
+            int x =
+            int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x);
+                if(x == 8)
+            {
+                Console.WriteLine("You Win!");
+            }
+                else if(x <= 8)
+            {
+                Console.WriteLine("You Lose");
+            }
+                else if (x >= 8)
+            {
+                Console.WriteLine("You Lose");
+            }
+            //now to input ternary oerator
+
+
+            var BOB = (x == 8) ? "You Win" : "You Lose";
+
+
+
+            Console.ReadLine();
         }
     }
 }
